e27ede4605c582085aa34de478d973caa6687fa7
Abhishek Srivastava <cabsriv@codeaurora.org>
2018-10-23 19:18:50 +0530

    QCSDK: Add support of wowlan_triggers, accept/deny_mac_file configuration.
    This allows setting of following configs to hostapd.conf through QSAP:
    wowlan_triggers accept_mac_file, deny_mac_file
    
    CRs-Fixed: 2336537
    Change-Id: If6a9e6a202e7aec86e1a3562bb392459a61287a2

--- softap/sdk/qsap_api.c
+++ softap/sdk/qsap_api.c
@@ -180,6 +180,12 @@ static struct Command cmd_list[eCMD_LAST] = {
     { "ssid2",                 NULL             },
     { "bridge",                NULL             },
     { "ctrl_interface",        NULL             },
+    { "vendor_elements",       NULL             },
+    { "assocresp_elements",    NULL             },
+    { "acs_exclude_dfs",       NULL             },
+    { "wowlan_triggers",       "any"            },
+    { "accept_mac_file",       NULL             },
+    { "deny_mac_file",         NULL             },
 };
 
 struct Command qsap_str[eSTR_LAST] = {
